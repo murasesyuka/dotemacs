40f859f6de7a49f6581bac753521186171519fdb
murase_syuka <murase.syuka+github@gmail.com>
2010-12-07 20:50:35 +0900

    update scala2.8.1

--- elisp/init-scala.el
+++ elisp/init-scala.el
@@ -1,7 +1,7 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;パス設定
-(add-to-list 'load-path "/opt/scala-2.8.0.final/misc/scala-tool-support/emacs/")
+(add-to-list 'load-path "/opt/scala-2.8.1.final/misc/scala-tool-support/emacs/")
 
 ;scala-mode
 (require 'scala-mode-auto)
