2b470064313bfd12f3cfc9d3addcaa5a6a8c197b
murase_syuka <murase.syuka+github@gmail.com>
2011-03-18 00:36:15 +0900

    fix flymake-mode, add c-mode.

--- elisp/init-flymake.el
+++ elisp/init-flymake.el
@@ -4,6 +4,22 @@
 ;;; Flymake
 (require 'flymake)
 
+;;; ; http://d.hatena.ne.jp/mowamowa/20071217/1197865281
+;;; ;; C
+(defun flymake-c-init ()
+  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+         (local-file  (file-relative-name
+                       temp-file
+                       (file-name-directory buffer-file-name))))
+    (list "gcc" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+(push '("\\.c$" flymake-c-init) flymake-allowed-file-name-masks)
+
+(add-hook 'c-mode-hook
+          '(lambda ()
+             (flymake-mode t)))
+
+
 ;;; ; http://d.hatena.ne.jp/suztomo/20080905/1220633281
 ;;; ;; C++
 (defun flymake-cc-init ()
