ec20a171d83e3e157ca1f79426830404e1e0e085
murase_syuka <murase.syuka+github@gmail.com>
2011-06-24 09:57:09 +0900

    add GDB.

---
+++ elisp/init-gdb.el
@@ -0,0 +1,16 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+; http://d.hatena.ne.jp/higepon/20090505/p1
+
+;;; GDB 関連
+;;; 有用なバッファを開くモード
+(setq gdb-many-windows t)
+
+;;; 変数の上にマウスカーソルを置くと値を表示
+(add-hook 'gdb-mode-hook '(lambda () (gud-tooltip-mode t)))
+
+;;; I/O バッファを表示
+(setq gdb-use-separate-io-buffer t)
+
+;;; t にすると mini buffer に値が表示される
+(setq gud-tooltip-echo-area nil)
