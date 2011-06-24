efc75c5633d40d7d4a63f87e9662eb7a13e6071d
murase_syuka <murase.syuka+github@gmail.com>
2011-06-24 09:54:17 +0900

    add coffee-script and shadow.el

---
+++ elisp/init-javascript.el
@@ -0,0 +1,24 @@
+;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
+
+
+
+;;; http://ozmm.org/posts/coffee_mode.html
+;;; git clone git://github.com/defunkt/coffee-mode.git
+;;; coffeescript-mode
+(add-to-list 'load-path "~/.emacs.d/elisp/coffee-mode")
+(require 'coffee-mode)
+
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+
+;;; http://d.hatena.ne.jp/mooz/20110504/p1
+;;; git clone https://github.com/mooz/shadow.el.git
+;;; shadow.el
+(add-to-list 'load-path "~/.emacs.d/elisp/shadow.el")
+(require 'shadow)
+
+(add-hook 'find-file-hooks 'shadow-on-find-file)
+(add-hook 'shadow-find-unshadow-hook
+          (lambda () (auto-revert-mode 1)))
+
