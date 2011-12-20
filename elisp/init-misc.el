c96d48aa4f70a89d4aa512df8c8fb67faa828efe
murase_syuka <murase.syuka+github@gmail.com>
2011-12-20 23:28:21 +0900

    add auto-async-byte-compile.el

--- elisp/init-misc.el
+++ elisp/init-misc.el
@@ -104,6 +104,16 @@
                'ielm-mode-hook
                ))
   (add-hook hook '(lambda () (paredit-mode 1))))
+(require 'auto-async-byte-compile)
+;; 自動バイトコンパイルを無効化にするファイル名の正規表現
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
+(setq eldoc-idle-delay 0.2)            ; すぐに表示したい
+(setq eldoc-minor-mode-string "")      ; モードラインにElDocと表示しない
+
 
 
 
