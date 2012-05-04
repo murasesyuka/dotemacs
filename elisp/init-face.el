4e292eef165605d46df8082186524de154e1e18e
murase_syuka <murase.syuka+github@gmail.com>
2012-05-04 23:41:40 +0900

    chg color, use color-theme.el

--- elisp/init-face.el
+++ elisp/init-face.el
@@ -1,12 +1,10 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
-(custom-set-faces
- '(default ((t
-             (:background "black" :foreground "#55FF55")))))
- '(cursor ((((class color)
-             (background dark))
-            (:background "#00AA00"))
-           (((class color)
-             (background light))
-            (:background "#999999"))
-           (t ())))
+(require 'color-theme)
+;(color-theme-katester)
+(color-theme-euphoria)
+
+
+
+
+
