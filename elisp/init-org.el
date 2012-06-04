feeda4cfc0fe0fec9211e6fbe15ee7946f5951ef
murase_syuka <murase.syuka+github@gmail.com>
2012-06-04 20:51:15 +0900

    fix org-mode

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -1,5 +1,23 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
+
+
+(require 'org-install)
+
+(require 'ob-ruby)
+(require 'ob-python)
+
+
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+
+(add-hook 'org-mode-hook 'turn-on-iimage-mode)
+
 ;; http://pastelwill.jp/wiki/doku.php?id=emacs:org-tree-slide
 ;; (auto-install-from-url "https://raw.github.com/takaxp/org-tree-slide/master/org-tree-slide.el")
 
