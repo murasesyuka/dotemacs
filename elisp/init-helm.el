e247b7e37204ad5ae56e7f3b42eaad6ed66062d7
murase_syuka <murase.syuka+github@gmail.com>
2012-11-15 21:55:20 +0900

    fix : disable helm-mode, it is hard to use.

--- elisp/init-helm.el
+++ elisp/init-helm.el
@@ -1,7 +1,7 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 (require 'helm-config)
-(helm-mode 1)
+;(helm-mode 1)
 
 (global-set-key (kbd "C-;") 'helm-mini)
 (global-set-key (kbd "C-z") 'helm-M-x)
