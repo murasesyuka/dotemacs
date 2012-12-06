1555dcf9219a08188f846f3ada5e358166476cb5
murase_syuka <murase.syuka+github@gmail.com>
2012-12-07 00:14:41 +0900

    fix : quickrun of c++ default is clang/c++11

--- elisp/init-quickrun.el
+++ elisp/init-quickrun.el
@@ -10,4 +10,14 @@
 
 ;; よく使うならキーを割り当てるとよいでしょう
 (global-set-key (kbd "<f5>") 'quickrun)
+(global-set-key (kbd "M-<f5>") 'quickrun-compile-only)
 
+;; Add C++ command for C11 and set it default in C++ file.
+(quickrun-add-command "c++/clang++"
+                      '((:command . "clang++")
+                        (:exec . ("%c -std=c++11 -lstdc++ %o -o %e %s"
+                                     "%e %a"))
+                        (:remove . ("%e")))
+                      :default "c++")
+
+(quickrun-set-default "c++" "c++/clang++")
