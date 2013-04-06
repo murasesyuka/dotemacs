28cefeff97b9512fc6f8fb2e7b201330da75f815
murase_syuka <murase.syuka+github@gmail.com>
2013-04-06 21:04:37 +0900

    fix : remove load path

--- elisp/init-yasnippet.el
+++ elisp/init-yasnippet.el
@@ -2,11 +2,11 @@
 
 (require 'yasnippet)
 (require 'helm-c-yasnippet)
+
 (setq helm-c-yas-space-match-any-greedy t) ;[default: nil]
 (global-set-key (kbd "C-c y") 'helm-c-yas-complete)
+
 (yas--initialize)
-(yas-load-directory "~/.emacs.d/elpa/yasnippet-20121108.2222/snippets")
-;(yas-load-directory "~/.emacs.d/elpa/yasnippet-20121108.2222/extras/imported")
 ;(add-to-list 'yas-extra-mode-hooks 'ruby-mode-hook)
 ;(add-to-list 'yas-extra-mode-hooks 'cperl-mode-hook)
 (yas-global-mode 1)
