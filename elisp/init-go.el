d8ee2ee72c8771cca2b45dbc1f8b9d86da7a9b97
murase_syuka <murase.syuka+github@gmail.com>
2014-01-19 15:31:36 +0900

    fix : load methods

--- elisp/init-go.el
+++ elisp/init-go.el
@@ -1,7 +1,8 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;; go mode
-;(setq load-path (cons "/usr/local/go/misc/emacs" load-path))
-(require 'go-mode-load)
 
+(eval-after-load "go-mode"
+  '(progn
+     (require 'go-autocomplete)))
 
