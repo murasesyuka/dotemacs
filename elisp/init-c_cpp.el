e8987513cc20a7ddbd275db90ee8ae1bd18d89e2
murase_syuka <murase.syuka+github@gmail.com>
2014-08-11 22:11:02 +0900

    change default c++ tab

--- elisp/init-c_cpp.el
+++ elisp/init-c_cpp.el
@@ -11,15 +11,28 @@
                 ("\\.cuh$"  . c++-mode)
                 ) auto-mode-alist))
 
+;; (setq-default c-basic-offset 4     ;;基本インデント量4
+;;               tab-width 4          ;;タブ幅4
+;;               indent-tabs-mode nil)  ;;インデントをタブでするかスペースでするか
+
 (add-hook 'c-mode-hook
           (lambda ()
             (set (make-local-variable 'eldoc-idle-delay) 0.20)
-	    (color-theme-arjen)
+	    (c-set-style "linux")
+	    (setq c-basic-offset 8)
+	    (setq tab-width 8)
+	    (setq indent-tabs-mode t)	; インデントは空白文字で行う（TABコードを空白に変換）
             (c-turn-on-eldoc-mode)
+	    (color-theme-arjen)
             ))
 
 (add-hook 'c++-mode-hook
           (lambda ()
-	    (color-theme-arjen)
+	    (c-set-style "stroustrup")
+	    (setq indent-tabs-mode nil)     ; インデントは空白文字で行う（TABコードを空白に変換）
+	    (setq c-basic-offset 4)
+	    (setq tab-width 4)
+	    ;; (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
+	    ;; (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
+	    (color-theme-snow)
             ))
-
